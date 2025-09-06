c33fb16ddbb435e81d53888dd863fc1bf9483764
Vladimir Maryasov <mx.wg@yandex.ru>
2025-09-07 05:13:23 +0700

    items script

--- HeroWarsHelperMod.user.js
+++ HeroWarsHelperMod.user.js
@@ -3,7 +3,7 @@
 // @name:en			HeroWarsHelperMod
 // @name:ru			HeroWarsHelperMod
 // @namespace		HeroWarsHelperMod
-// @version			2.366.25-09-07-05-07
+// @version			2.366.25-09-07-05-13
 // @description		Automation of actions for the game Hero Wars
 // @description:en	Automation of actions for the game Hero Wars
 // @description:ru	Автоматизация действий для игры Хроники Хаоса
@@ -2972,6 +2972,7 @@ async function checkChangeResponse(response) {
                        missionItemSearch = answer
                        if (answer) {
                          isChange = true;
+                         setIsCancalBattle(true);
                        }
                      }
 
